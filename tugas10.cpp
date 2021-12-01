218bbe7f8967d04d9b6d8d4e7ea9d58b5b165655
dedyalfaris <dedyalfaris.da41@gmail.com>
2021-12-02 00:15:44 +0700

    Tugas 10 C++
    Berikut Saya Lampirkan Tugas 10 C++ Kepada Niomic

---
+++ tugas10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void procedure(char perkalian){
+
+    cout<<"Selamat Datang Di Fungsi Perkalian"<<perkalian<<endl;
+
+}
+
+int main (){
+    char a;
+    procedure(a);
+
+}
